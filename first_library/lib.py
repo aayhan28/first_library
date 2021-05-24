e96435768a71b1846a775a60e02d88f44e879f56
Aytac Ayhan <aytac.ayhan@outlook.com>
2021-05-24 16:25:18 +0200

    Initial Push

---
+++ first_library/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    age = int(input('How old are you?'))
+    if age > 18:
+        print("You can enter, but behave!")
+    else:
+        print("Go back home, kid.")
+    
+try_me()
